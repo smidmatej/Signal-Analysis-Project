0bc458ff8e482bc5ecb98b2cfdb480bda513c91a
smidmatej <smidmatej99@gmail.com>
2022-11-10 13:53:03 +0100

    Added pitch up and down modification. TODO: fix frequency display iing incorrrect values and add robotisation

--- Vocodeur.m
+++ Vocodeur.m
@@ -96,13 +96,13 @@ sound(yrapide,Fs)
 %--------
 % Plot slowed sound
 Ylent = fft(yrapide);
-tlent = linspace(0,length(yrapide)-1,length(yrapide))*Ts;
+tvoc = linspace(0,length(yrapide)-1,length(yrapide))*Ts;
 % This is clunky, I have to multiply by rapp twice. That is because of the
 % linspace function. Whatever, it works
 flent = linspace(-length(yrapide)/2, length(yrapide)/2-1, length(yrapide))/(Fs/rapp/rapp);
 figure("Name", "Faster yrapide")
 subplot(1,3,1)
-plot(tlent,yrapide)
+plot(tvoc,yrapide)
 xlabel('Time')
 ylabel('Amplitude')
 subplot(1,3,2)
@@ -129,25 +129,62 @@ Nwind = Nfft;
 % Increase
 %--------------
 a = 2; b=3; %can be changed
-yvoc = PVoc(y, a/b,Nfft,Nwind);
+yvoc = PVoc(y, a/b, Nfft, Nwind);
 
 % Resampling the time signal to keep the same speed
-% TO DO !
+yvoc = resample(yvoc, a, b);
+
 
 %Sum of original and modified signal
 %Attention: we must take the same number of samples
 %Note: you can put a coefficient on ypitch so that it
 % intervenes + or - in the sum...
-% TO DO !
+
+% Resample the original signal for better comparison
+sum_sampling_rate = length(yvoc)/length(y);
+[n,d] = rat(sum_sampling_rate);
+y_compare = resample(y,n,d);
+y_sum = y_compare(1:length(yvoc))-yvoc;
 
 % Listen
 %-------
-% TO DO !
+sound(yvoc, Fs)
 
 % Curves
 %--------
-% TO DO !
 
+Yvoc = fft(yvoc);
+tvoc = linspace(0,length(yvoc)-1,length(yvoc))*Ts;
+% This is clunky, I have to multiply by rapp twice. That is because of the
+% linspace function. Whatever, it works
+flent = linspace(-length(yvoc)/2, length(yvoc)/2-1, length(yvoc))/(Fs);
+figure('Name', 'Pitch modificaton up')
+
+subplot(2,3,1)
+hold on
+plot(tvoc, yvoc)
+plot(tvoc, y_compare(1:length(yvoc)), '--')
+xlabel('Time')
+ylabel('Amplitude')
+legend({'$y_{voc}$', '$y$'}, 'interpreter', 'latex' ,'Location', 'Best')
+
+subplot(2,3,3+1)
+plot(y_sum)
+title('$y_{sum}$', 'interpreter', 'latex')
+xlabel('Time')
+ylabel('Amplitude')
+
+subplot(2,3,[2 3+2])
+plot(flent,abs(Yvoc).^2/length(y))
+xlabel('Frequency')
+ylabel('Power')
+subplot(2,3,[3 3+3])
+spectrogram(yvoc,128,120,128,Fs,'yaxis');
+
+
+
+
+%%
 
 %Decrease
 %------------
@@ -155,22 +192,59 @@ yvoc = PVoc(y, a/b,Nfft,Nwind);
 a=3; b=2; %can be changed
 yvoc = PVoc(y, a/b,Nfft,Nwind);
 
+
 % Resampling the time signal to keep the same speed
-% TO DO !
+yvoc = resample(yvoc, a, b);
+
 
 %Sum of original and modified signal
 %Attention: we must take the same number of samples
 %Note: you can put a coefficient on ypitch so that it
 % intervenes + or - in the sum...
-% TO DO !
+
+% Resample the original signal for better comparison
+sum_sampling_rate = length(yvoc)/length(y);
+[n,d] = rat(sum_sampling_rate);
+y_compare = resample(y,n,d);
+y_sum = y_compare(1:length(yvoc))-yvoc;
+
 
 % Listen
 %-------
-% TO DO !
+sound(yvoc, Fs)
 
 % Curves
 %--------
-% TO DO !
+
+% TODO: I dont think the frequency matches
+
+Yvoc = fft(yvoc);
+tvoc = linspace(0,length(yvoc)-1,length(yvoc))*Ts;
+% This is clunky, I have to multiply by rapp twice. That is because of the
+% linspace function. Whatever, it works
+flent = linspace(-length(yvoc)/2, length(yvoc)/2-1, length(yvoc))/(Fs);
+figure('Name', 'Pitch modificaton down')
+
+subplot(2,3,1)
+hold on
+plot(tvoc, yvoc)
+plot(tvoc, y_compare(1:length(yvoc)), '--')
+xlabel('Time')
+ylabel('Amplitude')
+legend({'$y_{voc}$', '$y$'}, 'interpreter', 'latex' ,'Location', 'Best')
+
+subplot(2,3,3+1)
+plot(y_sum)
+title('$y_{sum}$', 'interpreter', 'latex')
+xlabel('Time')
+ylabel('Amplitude')
+
+subplot(2,3,[2 3+2])
+plot(flent,abs(Yvoc).^2/length(y))
+xlabel('Frequency')
+ylabel('Power')
+subplot(2,3,[3 3+3])
+spectrogram(yvoc,128,120,128,Fs,'yaxis');
 
 
 %%
